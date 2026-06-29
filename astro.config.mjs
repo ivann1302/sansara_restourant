4584d992ec5171e06dcc4df74d803e86163de900
ivan <ivann97n@gmail.com>
2026-06-29 23:54:15 +0300

    ci: deploy site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://ivann1302.github.io",
+  base: "/sansara_restourant",
   integrations: [react()],
 });
