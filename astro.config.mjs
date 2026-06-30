e6def989aaae5226dc0b2e5b5ba5a2d06153ea23
ivan <ivann97n@gmail.com>
2026-06-30 10:18:00 +0300

    fix: -исправил баги на фронте

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+const isDev = process.env.NODE_ENV === "development";
+
 export default defineConfig({
   site: "https://ivann1302.github.io",
-  base: "/sansara_restourant",
+  base: isDev ? "/" : "/sansara_restourant",
   integrations: [react()],
 });
