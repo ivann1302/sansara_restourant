aad715bf1320c53b4dfd97b924e22679077c0c22
ivan <ivann97n@gmail.com>
2026-07-06 00:05:15 +0300

    perf: improve lighthouse score

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 const isDev = process.env.NODE_ENV === "development";
 
 export default defineConfig({
   site: "https://ivann1302.github.io",
   base: isDev ? "/" : "/sansara_restourant",
-  integrations: [react()],
 });
